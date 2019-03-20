058d80446843f1710ff03fada44e57fff75197fd
Blair Anderson <Blair81@gmail.com>
2019-03-20 08:56:38 -0700

    use scripts on this page

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function getTimeRemaining(endtime) {
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 / 60 / 60)) % 24);
-  var days = Math.floor(t / ((1000 / 60 / 60) * 24));
+  var days = Math.floor(t / (1000 * 24));
   return {
     total: t,
     days: days,
